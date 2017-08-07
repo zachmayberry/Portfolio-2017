be49084a6226a591d3185092310dcc14e4db3170
Zachary Mayberry <zachmayberry@gmail.com>
2017-08-07 08:18:01 -0500

    Saving state

--- src/components/EmailButton.js
+++ src/components/EmailButton.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react';
-//import EmailObfuscate from 'email-obfuscate';
+import EmailObfuscate from 'email-obfuscate';
 import './EmailButton.css';
 import Button from './Button';
 
 class EmailButton extends Component {
   componentDidMount() {
-    // var el = document.getElementById('email');
-    //
-    // EmailObfuscate(el, {
-    //   name: 'zach',
-    //   domain: 'zachmayberry',
-    //   tld: 'com',
-    //   altText: 'Email'
-    // });
-    //
-    // el.setAttribute('class', 'button');
+    var el = document.getElementById('email');
+
+    EmailObfuscate(el, {
+      name: 'zach',
+      domain: 'zachmayberry',
+      tld: 'com',
+      altText: 'Email'
+    });
+
+    el.setAttribute('class', 'button');
   }
 
   render() {
